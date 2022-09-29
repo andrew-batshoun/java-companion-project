5c8759adab72c9f2cab2d62e36617cc6ae0468d8
andrew.batshoun <andrew.batshoun@AFSWWL0S8LVCKO.us.accenturefederal.com>
2022-09-29 11:02:18 -0500

    created new package, interface, and updated service.js

--- mgl-webapp/src/main/java/com/organization/mvcproject/controller/HomeController.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/controller/HomeController.java
@@ -1,11 +1,39 @@
 package com.organization.mvcproject.controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.organization.mvcproject.model.GameImpl;
+import com.organization.mvcproject.model.ReviewImpl;
+
 @Controller
 public class HomeController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		return "index";
+	}
+
+	@RequestMapping(value = "/review", method = RequestMethod.GET)
+	public ModelAndView review() {
+		return new ModelAndView("reviewCreatePage", "command", new ReviewImpl());
+	}
+
+	@RequestMapping(value = "/addReview", method = RequestMethod.POST)
+	public ModelAndView addReview(ReviewImpl reviewImpl, ModelMap model) {
+		if(reviewImpl.getAuthor().equals("")) {
+			reviewImpl.setAuthor("anonymous");
+		}
+		return new ModelAndView("reviewDetailPage", "submittedReview", reviewImpl);
+	}
+
+	@RequestMapping(value = "/games", method = RequestMethod.GET)
+	public ModelAndView game() {
+		return new ModelAndView("gamesPage", "command", new GameImpl());
+	}
    
 	@RequestMapping(value="/hello")
     public ModelAndView hello(@RequestParam(required=false, defaultValue="World") String name) {
